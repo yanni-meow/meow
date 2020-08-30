16c18a863ecdd1c8a6c401c7be5466651b061490
yanni <yanni.mamporiya@gmail.com>
2020-08-30 22:36:56 +0300

    slider, map

---
+++ firstproj/js/js/googlemap.js
@@ -0,0 +1,11 @@
+function initMap() {
+    // The location of Moscow
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
+
+//   55.5807481,36.8251304,9
